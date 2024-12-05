95095d16ad486a94fe356979c72a73f3b2477fb6
SimoLink <fenech.simone@gmail.com>
2024-12-05 19:02:15 +0100

    Minor fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+import { useMediaQuery } from 'react-responsive';
+
 function App() {
+  // Media query per il tablet (Microsoft Surface in verticale)
+  const isTablet = useMediaQuery({ query: '(max-width: 1824px) and (min-width: 600px)' });
+
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<div>LUMI NON VIOLA NESSUN COPYRIGHT, PER FAVORE NINTENDO NON FARCI CAUSA (SE PROPRIO DEVI CERCA REBECCA AUDISIO)</div>} />
-      </Routes>
+      {isTablet ? (
+        <div>Vista per Tablet</div>
+      ) : (
+        <div>INSERIRE IMMAGINE CATTIVA DI LUMI CHE DICE DI RUOTARE IL TABLET</div>
+      )}
     </div>
   );
 }
