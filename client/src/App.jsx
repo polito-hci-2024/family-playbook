992033971d87d29758c95ad061642333c031cff0
SimoLink <fenech.simone@gmail.com>
2024-12-05 19:04:43 +0100

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,12 @@ import { useMediaQuery } from 'react-responsive';
 
 function App() {
   // Media query per il tablet (Microsoft Surface in verticale)
-  const isTablet = useMediaQuery({ query: '(max-width: 1824px) and (min-width: 600px)' });
+  const isTablet = useMediaQuery({ query: '(max-width: 1824px) and (min-width: 2736px)' });
 
   return (
     <div>
       {isTablet ? (
-        <div>Vista per Tablet</div>
+        <div>Vista per Microsoft Surface 7 Pro</div>
       ) : (
         <div>INSERIRE IMMAGINE CATTIVA DI LUMI CHE DICE DI RUOTARE IL TABLET</div>
       )}
