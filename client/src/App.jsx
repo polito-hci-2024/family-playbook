d191f34d05f822f7eb43c300b6573d746b2c9df1
RebeccaAudisio <s260537@studenti.polito.it>
2024-12-09 17:57:17 +0100

    opening page implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavHeader from './components/NavHeader';
 import { Container } from 'react-bootstrap';
-import NotFound  from './components/NotFound';
-import WelcomePage  from './components/WelcomePage';
-import { Routes, Route, Outlet } from "react-router-dom";
+import NotFound from './components/NotFound';
+import WelcomePage from './components/WelcomePage';
+import { Routes, Route, Outlet } from 'react-router-dom';
+import OpeningPage from './components/OpeningPage';
 
 function App() {
   const [isTablet, setIsTablet] = useState(false);
@@ -32,18 +33,23 @@ function App() {
     <div>
       {isTablet ? (
         <Routes>
+          {/* Questa route è per la pagina OpeningPage senza NavHeader */}
+          <Route path="/" element={<OpeningPage />} />
+          
+          {/* Wrapper per tutte le altre pagine, con il NavHeader */}
           <Route
             element={
               <>
                 <NavHeader />
                 <Container>
-                  <Outlet />
+                  {/* Outlet renderizzerà il contenuto delle rotte figlie */}
                 </Container>
               </>
             }
           >
-            {/* Home */}
-            <Route path="/" element={<WelcomePage />} />
+            {/* Altre rotte che includono il NavHeader */}
+            <Route path="/welcome" element={<WelcomePage />} />
+            {/* Puoi aggiungere altre rotte qui per le pagine che devono avere il NavHeader */}
 
             {/* Pagina non trovata */}
             <Route path="*" element={<NotFound />} />
@@ -54,7 +60,6 @@ function App() {
           <p>
             <strong>Lumi dice:</strong> Per favore, usa un tablet o ruota il tuo dispositivo!
           </p>
-          {/* Inserisci un'immagine di Lumi se disponibile */}
         </div>
       )}
     </div>
