b4c60fdd339a289a43f1f06e79b879f5892e9681
SimoLink <fenech.simone@gmail.com>
2024-12-11 19:05:00 +0100

    Minor fix

--- client/src/API.mjs
+++ client/src/API.mjs
@@ -3,11 +3,8 @@ const SERVER_URL = 'http://localhost:3001';
 // Activities
 const getActivities = async () => {
     const response = await fetch(SERVER_URL + '/api/activities');
-    console.log(response, "ciao");
-
     if(response.ok) {
         const activitiesJson = await response.json();
-        console.log(activitiesJson);
         return activitiesJson;
     }
     else 
