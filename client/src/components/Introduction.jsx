a5662cfd8babf8b15a02581f25806b931165a2ca
RebeccaAudisio <s260537@studenti.polito.it>
2024-12-17 00:52:46 +0100

    ccc

--- client/src/components/Introduction.jsx
+++ client/src/components/Introduction.jsx
@@ -1,6 +1,6 @@
 import React,  { useEffect }  from 'react';
 import '../CSS/Introduction.css';
-import { Image } from 'react-bootstrap';
+import { Image, Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom'; 
 import '../CSS/OpeningPage.css';
 import '../CSS/Carousel.css';
@@ -15,23 +15,42 @@ function StoryPage({ characterName, characterType }) {
 
 
   return (
-    <div className="legno">
+    <div className="Introduction">
+      <Image 
+        src="/img/Untitled design.png"  // Percorso corretto
+        fluid 
+        className="story-background"  // Usa la stessa classe definita nel CSS
+      />
       <div className="story-background">
-      <div className="story-content">
-        <div className="title"> <h1>Chapter 1</h1> </div>
-        <p className="story-text"> {`C'era una volta un giovane mago di nome Leo, i cui poteri, sebbene in fase di perfezionamento, racchiudevano già una promessa di grandezza. Cresciuto in una piccola cittadina, ai margini di una foresta immensa e misteriosa, Leo era circondato dalle rovine di antichi castelli e leggende dimenticate.`}</p>
-        <p className="story-text"> {`Nel cuore di quel paesaggio selvaggio, aveva imparato l'arte della magia antica, trasmessa da generazioni, ma nessuno gli aveva mai parlato delle terre lontane e sconosciute che si celavano oltre l'orizzonte.`}</p>
-        <p className="story-text"> {`Un giorno, perso nei suoi pensieri e nel vortice di domande che lo accompagnavano, Leo si ritrovò più lontano del solito dalla sua casa. \n Improvvisamente, senza rendersene conto, si trovò davanti a un bivio, là dove la strada si divideva in due direzioni misteriose. `}</p>
-        <Image
-          src={`/img/characters/wizard.png`} // Adatta il percorso per i personaggi
-          alt={characterName}
-          className="character-image"
-        />
-      </div>
+        <div className="story-content">
+          <div className="title">
+            <h1>Chapter 1</h1>
+          </div>
+          <div className="text-with-image">
+          <img 
+              src="/img/characters/66a0f748dcb9b5de89b29e401162588b-removebg-preview-removebg-preview.png" 
+              alt="Leo il mago" 
+              className="inline-image"
+            />
+            <p className="story-text" sstyle={{ display: 'flex', flexDirection: 'row' }} >
+                {`C'era una volta un giovane mago di nome Leo, i cui poteri, sebbene in fase di perfezionamento, racchiudevano già una promessa di grandezza. Cresciuto in una piccola cittadina, ai margini di una foresta immensa e misteriosa, Leo era circondato dalle rovine di antichi castelli e leggende dimenticate.`}
+              </p>
+            
+          </div>
+          <p className="story-text">
+            {`Nel cuore di quel paesaggio selvaggio, aveva imparato l'arte della magia antica, trasmessa da generazioni, ma nessuno gli aveva mai parlato delle terre lontane e sconosciute che si celavano oltre l'orizzonte.`}
+          </p>
+          <p className="story-text">
+            {`Un giorno, perso nei suoi pensieri e nel vortice di domande che lo accompagnavano, Leo si ritrovò più lontano del solito dalla sua casa.`}
+          </p>
+          <p className="story-text">
+            {`Improvvisamente, senza rendersene conto, si trovò davanti a un bivio, là dove la strada si divideva in due direzioni misteriose.`}
+          </p>
+        </div>
       </div>
     </div>
   );
-};
+};  
 
 export default StoryPage;
 
