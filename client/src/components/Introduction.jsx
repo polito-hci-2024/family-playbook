64bf6a515dc615e47c5a19ba39105dfde309cf4b
RebeccaAudisio <s260537@studenti.polito.it>
2024-12-17 18:53:56 +0100

    prima pagina introduzione

--- client/src/components/Introduction.jsx
+++ client/src/components/Introduction.jsx
@@ -16,41 +16,42 @@ function StoryPage({ characterName, characterType }) {
 
   return (
     <div className="Introduction">
-      <Image 
-        src="/img/Untitled design.png"  // Percorso corretto
-        fluid 
-        className="story-background"  // Usa la stessa classe definita nel CSS
-      />
       <div className="story-background">
         <div className="story-content">
           <div className="title">
             <h1>Chapter 1</h1>
           </div>
-          <div className="text-with-image">
-          <img 
-              src="/img/characters/66a0f748dcb9b5de89b29e401162588b-removebg-preview-removebg-preview.png" 
-              alt="Leo il mago" 
+  
+          <div className="inline-container">
+            <img
+              src="/img/characters/66a0f748dcb9b5de89b29e401162588b-removebg-preview-removebg-preview.png"
+              alt="Leo il mago"
               className="inline-image"
             />
-            <p className="story-text" sstyle={{ display: 'flex', flexDirection: 'row' }} >
-                {`C'era una volta un giovane mago di nome Leo, i cui poteri, sebbene in fase di perfezionamento, racchiudevano già una promessa di grandezza. Cresciuto in una piccola cittadina, ai margini di una foresta immensa e misteriosa, Leo era circondato dalle rovine di antichi castelli e leggende dimenticate.`}
-              </p>
-            
+            <p className="story-text" style={{ marginLeft: '210px' }}>
+              {`C'era una volta un giovane mago di nome Leo, i cui poteri, sebbene in fase di perfezionamento, racchiudevano già una promessa di grandezza. Cresciuto in una piccola cittadina, ai margini di una foresta immensa e misteriosa, Leo era circondato dalle rovine di antichi castelli e leggende dimenticate.\n`}
+            </p>
           </div>
+  
           <p className="story-text">
-            {`Nel cuore di quel paesaggio selvaggio, aveva imparato l'arte della magia antica, trasmessa da generazioni, ma nessuno gli aveva mai parlato delle terre lontane e sconosciute che si celavano oltre l'orizzonte.`}
+            {`\nNel cuore di quel paesaggio selvaggio, aveva imparato l'arte della magia antica, trasmessa da generazioni, ma nessuno gli aveva mai parlato delle terre lontane e sconosciute che si celavano oltre l'orizzonte.`}
           </p>
-          <p className="story-text">
-            {`Un giorno, perso nei suoi pensieri e nel vortice di domande che lo accompagnavano, Leo si ritrovò più lontano del solito dalla sua casa.`}
+          <p className="story-text" style={{  marginTop: '30px' }}>
+            {`\nUn giorno, perso nei suoi pensieri e nel vortice di domande che lo accompagnavano, Leo si ritrovò più lontano del solito dalla sua casa.`}
           </p>
-          <p className="story-text">
+          <p className="story-text" style={{ marginRight: '210px', marginTop: '30px' }}>
             {`Improvvisamente, senza rendersene conto, si trovò davanti a un bivio, là dove la strada si divideva in due direzioni misteriose.`}
           </p>
+          <img
+              src="/img/bivio.png"
+              alt="Bivio"
+              className="inline-image-bivio"
+            />
         </div>
       </div>
     </div>
   );
-};  
+}
 
 export default StoryPage;
 
