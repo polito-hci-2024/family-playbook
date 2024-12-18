a2d948b8c76b11551b1f19433a13e7987fc9e354
SABRINA BASILE s329161 <s329161@studenti.polito.it>
2024-12-18 12:38:19 +0100

    Insert character images and update WelcomePage

--- client/src/components/WelcomePage.jsx
+++ client/src/components/WelcomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-import { Container, Row, Col, Image, Button } from 'react-bootstrap';
+import { Container, Row, Col, Image, Button, Form } from 'react-bootstrap';
 
 import '../CSS/WelcomePage.css';
 
@@ -23,10 +23,18 @@ function WelcomePage() {
         </Col>
       </Row>
       <Row style={{ marginBottom: '20px' }}><CharacterSelector></CharacterSelector></Row>
-      <Row>
-        <h2>What's your name?</h2>
+      <Row className='mt-4 mb-4'>
+        <Col className='d-flex flex-column'>
+          <label style={{ fontFamily: 'fantasy', fontSize: '30px' }}>What's your name?</label>
+          <input type="text" name='name_character' placeholder='Insert your name' style={{ fontFamily: 'fantasy', height: '50px', fontSize: '25px' }}></input>
+        </Col>
       </Row>
-    </Container>
+      <Row className="mt-auto">
+        <Col className="d-grid">
+          <Button variant="success" className='btn-lg button-style'>Confirm</Button>
+        </Col>
+      </Row>
+    </Container >
   );
 }
 
@@ -34,23 +42,29 @@ function CharacterSelector() {
   const [selectedCharacter, setSelectedCharacter] = useState(null);
 
   const characters = [
-    { id: 1, name: 'Wizard', img: '../../img/lumi.png' },
-    { id: 2, name: 'Witch', img: '../../img/lumi.png' },
-    { id: 3, name: 'Dragon', img: '../../img/lumi.png' },
-    { id: 4, name: 'Fairy', img: '../../img/lumi.png' },
-    { id: 5, name: 'Elf', img: '../../img/lumi.png' },
-    { id: 6, name: 'Orc', img: '../../img/lumi.png' },
+    { id: 1, name: 'Wizard', img: '../../img/character_image/wizard.jpg' },
+    { id: 2, name: 'Witch', img: '../../img/character_image/witch.jpg' },
+    { id: 3, name: 'Dragon', img: '../../img/character_image/dragon.jpg' },
+    { id: 4, name: 'Fairy', img: '../../img/character_image/fairy.jpg' },
+    { id: 5, name: 'Elf', img: '../../img/character_image/elf.jpg' },
+    { id: 6, name: 'Orc', img: '../../img/character_image/orc.jpg' },
   ];
 
   const handleCharacterSelect = (id) => {
-    setSelectedCharacter(id);
+    // Se il personaggio selezionato è già selezionato, deseleziona (imposta a null)
+    if (selectedCharacter === id) {
+      setSelectedCharacter(null);
+    } else {
+      // Altrimenti seleziona il nuovo personaggio
+      setSelectedCharacter(id);
+    }
   };
 
   return (
     <Container>
       <Row>
         {characters.map((character) => (
-          <Col key={character.id} className="text-center" sm={4}>
+          <Col key={character.id} className="text-center col-sm-4">
             <Button
               className={`character-btn ${selectedCharacter === character.id ? 'selected' : ''}`}
               onClick={() => handleCharacterSelect(character.id)} variant='light'
