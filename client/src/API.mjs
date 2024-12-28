c1e82861c9695994f774a5f6a4bc49f4f6b4d558
Ascherimovie3 <ascherianita@gmail.com>
2024-12-28 12:42:42 +0100

    b

--- client/src/API.mjs
+++ client/src/API.mjs
@@ -1,8 +1,8 @@
 const SERVER_URL = 'http://localhost:3001';
 
 // INSERIRE FETCH
-const getChallengesById = async () => {
-  const response = await fetch(SERVER_URL + '/api/challenges');
+const getChallengesById = async (activity_id) => {
+  const response = await fetch(SERVER_URL + '/api/challenges/${activity_id}');
   if(response.ok) {
       const challengesJson = await response.json();
       return challengesJson;
