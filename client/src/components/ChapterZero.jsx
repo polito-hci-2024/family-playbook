250d9988c89dc837fbd66678459575eb6bd6b3bd
SabrinaBasile <basile.sabrina01@gmail.com>
2025-02-01 23:17:24 +0100

    Add video for guide in the Chapter Zero

--- client/src/components/ChapterZero.jsx
+++ client/src/components/ChapterZero.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import { motion } from 'framer-motion';
+import { useState } from 'react';
 import '../CSS/ChapterZero.css';
 import FormCharacterPage from './FormCharacterPage';
+import ModalGuide from './ModalGuide'
 
 function ChapterZero() {
+
 
   return (
     <div className="background-component">
       <h1 className="title">Chapter Zero</h1>
       <motion.div initial={{ opacity: 0, y: 20 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        transition={{ delay:  0.2, duration: 1 }}>
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.2, duration: 1 }}>
         <Container fluid className="character-panel">
+          <ModalGuide />
           <Row>
             <Col>
               <FormCharacterPage></FormCharacterPage>
@@ -21,8 +25,8 @@ function ChapterZero() {
         </Container>
       </motion.div>
       <div>
-          <img src='../../img/lumi_smile.png' className='lumi'></img>
-          <img src='../../img/cloudWelcome.png' className='cloud'></img>
+        <img src='../../img/lumi_smile.png' className='lumi'></img>
+        <img src='../../img/cloudWelcome.png' className='cloud'></img>
       </div>
     </div >
   );
